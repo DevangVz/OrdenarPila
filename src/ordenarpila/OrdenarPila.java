0dac9e85b15dec91238094d642683c954e0d8cf6
DevangVz <devang.vazquez@gmail.com>
2016-10-03 01:58:49 -0500

    githubFirst

---
+++ src/ordenarpila/OrdenarPila.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordenarpila;
+
+/**
+ *
+ * @author Devang
+ */
+public class OrdenarPila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] a = {5, 3, 1, 2, 4};
+        Pila pilaN = new Pila(a);
+        Pila pila2 = new Pila();
+        pila2.push(3);
+        pila2.push(5);
+        pila2.push(80);
+        pila2.push(2);
+        pila2.push(1);
+        pila2.push(6);
+        pila2.push(5);
+        pila2.push(56);
+        pila2.print();
+        pilaN.print();
+
+        OrdenarPila.order(pila2);
+        OrdenarPila.order(pilaN);
+    }
+
+    public static void order(Pila array) {
+        Pila beta = new Pila();
+        int chi = 0, index = 0;
+        do {
+            if (index == 0) {
+                beta.push(array.get());
+                array.pop();
+                index = 1;
+
+            } else {
+                chi = array.get();
+                array.pop();
+
+                while (!(beta.isEmpty()) && chi < beta.get()) {
+                    array.push(beta.get());
+                    beta.pop();
+                    index++;
+                }
+                beta.push(chi);
+            }
+        } while (!array.isEmpty());
+        beta.print();
+    }
+}
---
+++ src/ordenarpila/Pila.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordenarpila;
+
+/**
+ *
+ * @author Devang
+ */
+public class Pila {
+    int alfa=0;
+    int[] array;
+    
+    public Pila(){
+        array=new int[1];
+    }
+    public Pila(int[] n){
+        array= n;
+        alfa=n.length;
+    }
+    
+    
+    public void push(int n){
+        if(isFull()){
+            resize();
+        }else{
+        
+        }
+        array[alfa]=n;
+        alfa++;
+    }
+    
+    public void pop(){
+        alfa--;
+        if(!isEmpty()){
+            array[alfa]=0;
+        }
+        
+    }
+    public int get(){
+    try{        return (!isEmpty())?array[alfa-1]:0;}
+    catch(Exception e){
+    return 0;}
+    }
+    public boolean isEmpty(){
+        return (alfa==0);
+    }
+    
+    public boolean isFull(){
+        return  (alfa==array.length);
+    }
+    public void resize(){
+        int[] arrayAux= new int[array.length+1];
+        for(int i=0;i<array.length;i++){
+            arrayAux[i]=array[i];
+        }
+        array= arrayAux;
+    }
+    
+    public void print(){
+        System.out.println("*******");
+        for(int i=0; i<alfa;i++){
+            System.out.println(array[i]);
+        }
+    }
+    
+    public int length(){
+        return array.length;
+    }
+}
